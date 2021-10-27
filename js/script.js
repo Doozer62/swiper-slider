b08ca5fe0d682ac1736859533d6b92ab5fd0e006
Doozer62 <a.a.kopiev@mail.ru>
2021-10-27 11:38:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true
+    },
+});
